118451e23c57be7c637a9e2d52e52e5fc559f140
walter <buczacka.walter@gmail.com>
2018-03-14 18:30:26 -0300

    add webpack config

---
+++ webpack.config-test.js
@@ -0,0 +1,19 @@
+var nodeExternals = require('webpack-node-externals')
+module.exports = {
+  target: 'node',
+  externals: [nodeExternals()],
+    module: {
+    loaders: [
+      {
+        test:/\.js$/,
+        loader:"babel-loader",
+        query: {
+          presets: [
+            'es2015',
+            'stage-2'
+          ]
+        }
+      }
+    ]
+  }
+}
